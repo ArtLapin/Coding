75f3ddae81c65237097eb1cc696dd65b09e4c3f3
ArtLapin <101849460+ArtLapin@users.noreply.github.com>
2023-02-06 22:18:37 +0900

    Delete final_project.py

--- final_project.py
+++
@@ -1,8 +0,0 @@
-# requests 패키지 불러오기 (pip install requests 설치 필요)
-import requests
-
-# 네이버 영화 사이트 - 탑건: 매버릭 리뷰 페이지 
-url = "https://movie.naver.com/movie/bi/mi/review.naver?code=81888"
-
-# requests.get
-resp = None
